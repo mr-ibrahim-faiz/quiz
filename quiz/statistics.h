4ef32c9ae214351b2c4b70ceeb6e18fa6e3a4d52
mr-ibrahim-faiz <mr.ibrahim.faiz@gmail.com>
2020-11-20 10:38:44 +0100

    add statistics.h

---
+++ quiz/statistics.h
@@ -0,0 +1,24 @@
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+#include<vector>
+using std::vector;
+
+// user-defined type Statistics
+struct Statistics {
+	// default constructor
+	Statistics() noexcept
+		: successes {}
+		, failures {}{}
+
+	// constructor taking number of successes and failures as arguments
+	explicit Statistics(const vector<size_t>& successes, const vector<size_t>& failures) noexcept
+		: successes { successes }
+		, failures { failures }{}
+
+	// statistics information
+	vector<size_t> successes; // number of times questions were well answered
+	vector<size_t> failures; // number of times questions were not well answered
+};
+
+#endif
